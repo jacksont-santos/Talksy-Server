10866a6fcb1b8d75ccc3ce4c428a2c80fe2045ec
Jackson Santos <jackson.santos@kukac.com.br>
2026-03-29 19:35:58 -0300

    sending again

--- src/utils/appError.ts
+++ src/utils/appError.ts
@@ -2,7 +2,7 @@ export class AppError {
     public readonly statusCode: number;
     public readonly message: string | string[];
   
-    constructor(statusCode: number, message?: string | string[],) {
+    constructor(statusCode: number, message?: string | string[]) {
       this.statusCode = statusCode;
       this.message = message;
     }
